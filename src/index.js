e28ab90005a034e41563b444522023a3c9796c43
Vadim Shatalov <vadimshatalov@gmail.com>
2016-11-13 01:29:33 +0400

    exsercise 2B

--- src/index.js
+++ src/index.js
@@ -15,6 +15,43 @@ app.get('/task2A', (req, res) => {
   res.send(sum.toString())
 });
 
+app.get('/task2B', (req, res) => {
+  var fullname = req.query.fullname
+  if (fullname) {
+    fullname = fullname.split(' ')
+    var counter = fullname.length,
+        result = '';
+    if (counter > 3) {
+      res.send('Invalid fullname')
+    }
+
+    var family = fullname[counter-1],
+        name = '',
+        surname = '';
+    if (counter == 3) {
+      name = fullname[0]
+      surname = fullname[1]
+    } else if (counter == 2) {
+      name = fullname[0]
+    }
+
+    result = family
+    if (name) {
+      result = result + ' ' + name[0] + '.'
+    }
+    if (surname) {
+      result = result + ' ' + surname[0] + '.'
+    }
+
+
+    res.send(result)
+  } else {
+    res.send('Invalid fullname')
+  }
+
+});
+
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
